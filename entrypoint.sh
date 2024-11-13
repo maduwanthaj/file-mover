2ee80ac3cbd4b043fb521bb7290aad8bdf147054
maduwanthaj <jayasuriyamaduwantha@gmail.com>
2024-11-13 12:53:16 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 source /app/lib.sh
 
 # function to display usage instructions
-usage() {
+show_usage() {
     printf "${C_BOLD}Usage:${C_RESET}  entrypoint.sh [OPTIONS] [ARG...]\n\n"
     printf "${C_BOLD}Options:${C_RESET}\n"
     printf "${C_BLUE}%-20s${C_RESET} %-50s\n" "--schedule [TIME]" "schedule the container to run repeatedly at a specified time."
@@ -12,6 +12,7 @@ usage() {
     printf "${C_BLUE}%-20s${C_RESET} %-50s\n" "--help" "display this help message."
     printf "${C_BLUE}%-20s${C_RESET} %-50s\n" "--version" "display the script version."
     printf "\n"
+    exit 0
 }
 
 # function to add cron job based on input type
@@ -20,37 +21,51 @@ set_cron_job() {
     log_info "${2} file-moving task has been created."
 }
 
+# function to print the script version
+show_version() {
+    printf "${C_BOLD}Version:${C_RESET} ${VERSION}\n\n"
+    exit 0
+}
+
 # function to execute the main application script directly
-app() {
+execute_app() {
     sh /app/app.sh
+}
+
+# function to create a one-time task or schedule it at a specified time
+create_one_time_task() {
+    if [ -n "${1}" ]; then
+        touch "${SENTINEL}"
+        set_cron_job "${1}" "one-time"
+    else
+        log_info "immediate file-moving task has been created."
+        execute_app
+        exit 0
+    fi
+}
+
+# function to create a recurring task based on a specified time
+create_schedule_task() {
+    if [ -n "${1}" ]; then
+        set_cron_job "${1}" "scheduled"
+    else
+        show_usage
+        exit 0
+    fi
 }
 
 # parse command-line arguments and execute appropriate functions
 case "${1}" in
     "--help")
-        usage
-        exit 0 ;;
+        show_usage ;;
     "--version")
-        echo "Version: ${VERSION}"
-        exit 0 ;;
+        show_version ;;
     "--run-once")
-        if [ -n "${2}" ]; then
-            touch "${SENTINEL}"
-            set_cron_job "${2}" "one-time"
-        else
-            app
-            exit 0
-        fi ;;
+        create_one_time_task "${2}" ;;
     "--schedule")
-        if [ -n "${2}" ]; then
-            set_cron_job "${2}" "scheduled"
-        else
-            usage
-            exit 0
-        fi ;;
+        create_schedule_task "${2}" ;;
     *)
-        usage
-        exit 0 ;;
+        show_usage ;;
 esac
 
 # start cron in the foreground
